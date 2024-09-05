b3d119f2b8748f7c500fa598e71e8bfbb8a7e89d
Emily <84556633+DrPunkToad@users.noreply.github.com>
2024-09-04 18:03:10 -0700

    Add files via upload

---
+++ boolean_var.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+
+int main()
+{
+    std::cout << false << std::endl;
+    std::cout << !false << std::endl;
+
+    std::cout << std::boolalpha;
+     
+    std::cout << true << std::endl;
+    std::cout << true << std::endl;
+
+    return 0;
+
+}
